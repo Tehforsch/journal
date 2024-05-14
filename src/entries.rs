5d0347b9fe32cff1242e2ef8b49740674a4fa66c
tehforsch <mail@tonipeter.de>
2024-05-14 15:07:10 +0200

    add templating

--- src/entries.rs
+++ src/entries.rs
@@ -1,5 +1,8 @@
+use rand::prelude::SliceRandom;
 use std::io::Result;
 use std::path::{Path, PathBuf};
+
+use crate::config;
 
 #[derive(Debug)]
 pub struct Entry {
@@ -13,15 +16,38 @@ impl Entry {
             Ok(entries) => entries
                 .filter_map(move |e| {
                     let fname = e.unwrap().file_name().to_str().unwrap().to_owned();
-                    Some(Entry::read(&path.join(fname)))
+                    let fname =
+                        pathdiff::diff_paths(path.join("pics").join(&fname), config::JOURNAL_PATH)
+                            .unwrap();
+                    Some(Path::new("journal").join(fname))
                 })
                 .collect(),
             Err(_) => vec![],
         };
         Self {
-            text_path: path.to_owned(),
-            pics: vec![],
+            text_path: (path.join("entry.md")).to_owned(),
+            pics,
         }
+    }
+
+    pub(crate) fn content(&self) -> Result<String> {
+        std::fs::read_to_string(&self.text_path)
+    }
+
+    pub(crate) fn date_str(&self) -> String {
+        let filename = self
+            .text_path
+            .parent()
+            .unwrap()
+            .file_name()
+            .unwrap()
+            .to_str()
+            .unwrap();
+        filename.replace(".md", "")
+    }
+
+    pub fn pics(&self) -> &[PathBuf] {
+        self.pics.as_ref()
     }
 }
 
@@ -34,12 +60,17 @@ impl Entries {
     pub fn read(path: &Path) -> Result<Self> {
         let path = path.to_owned();
         let read_dir = std::fs::read_dir(path.clone())?;
-        let entries = read_dir
+        let entries: Vec<_> = read_dir
             .filter_map(move |e| {
                 let fname = e.unwrap().file_name().to_str().unwrap().to_owned();
                 Some(Entry::read(&path.join(fname)))
             })
             .collect();
         Ok(Self { entries })
+    }
+
+    pub(crate) fn random(&self) -> Option<&Entry> {
+        let mut thread_rng = rand::thread_rng();
+        self.entries.choose(&mut thread_rng)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,106 @@
 use std::path::Path;
 
+use entries::Entry;
+use rouille::{router, Response};
+use tera::Tera;
+
 use crate::entries::Entries;
 
 mod config;
 mod entries;
 
-// use rouille::Response;
+struct Manager {
+    entries: Entries,
+    tera: Tera,
+}
 
 fn main() {
     println!("Now listening on localhost:8000");
 
-    let entries = Entries::read(&Path::new(config::JOURNAL_PATH));
-    dbg!(&entries);
+    let entries = Entries::read(&Path::new(config::JOURNAL_PATH)).unwrap();
+    let mut tera = Tera::default();
+    // Embedding these here for simplicity, so I can just run the binary from anywhere
+    tera.add_raw_template("pic.html", include_str!("../templates/pic.html"))
+        .unwrap();
+    tera.add_raw_template("entry.html", include_str!("../templates/entry.html"))
+        .unwrap();
+    tera.add_raw_template(
+        "dashboard.html",
+        include_str!("../templates/dashboard.html"),
+    )
+    .unwrap();
+    tera.autoescape_on(vec![]);
+    let manager = Manager { entries, tera };
 
-    // rouille::start_server("localhost:8000", move |request| {
-    //     {
-    //         let response = rouille::match_assets(&request, ".");
-    //         if response.is_success() {
-    //             return response;
-    //         }
-    //     }
+    rouille::start_server("localhost:8000", move |request| {
+        {
+            router!(request,
+                (GET) (/dashboard) => {
+                    manager.dashboard()
+                },
+                _ => {
+                    let response = rouille::match_assets(&request, config::JOURNAL_PATH);
+                    if response.is_success() {
+                        response
+                    }
+                    else {
+                    Response::html(
+                        "404 error.",
+                    )
+                    .with_status_code(404)
+                    }
+                }
+            )
+        }
+    });
+}
 
-    //     Response::html(
-    //         "404 error. Try <a href=\"/README.md\"`>README.md</a> or \
-    //                     <a href=\"/src/lib.rs\">src/lib.rs</a> for example.",
-    //     )
-    //     .with_status_code(404)
-    // });
+impl Manager {
+    fn dashboard(&self) -> Response {
+        Response::html(self.dashboard_html())
+    }
+
+    fn dashboard_html(&self) -> String {
+        let mut context = tera::Context::new();
+        let num_entries = 3;
+        let result = (0..num_entries)
+            .map(|_| {
+                let random_entry = self.entries.random();
+                random_entry
+                    .map(|entry| self.entry_html(entry))
+                    .unwrap_or("".to_owned())
+            })
+            .collect::<Vec<_>>()
+            .join("\n");
+        context.insert("entries", &result);
+        self.tera.render("dashboard.html", &context).unwrap()
+    }
+
+    fn entry_html(&self, entry: &Entry) -> String {
+        let mut context = tera::Context::new();
+        context.insert(
+            "content",
+            &entry.content().unwrap().replace("\n", "\n<br/>"),
+        );
+        context.insert("date", &entry.date_str());
+        context.insert("pics", &self.pics_html(entry));
+        self.tera.render("entry.html", &context).unwrap()
+    }
+
+    fn pics_html(&self, entry: &Entry) -> String {
+        if entry.pics().is_empty() {
+            "".to_string()
+        } else {
+            entry
+                .pics()
+                .iter()
+                .map(|pic| {
+                    let mut context = tera::Context::new();
+                    context.insert("pic", pic);
+                    self.tera.render("pic.html", &context).unwrap()
+                })
+                .collect::<Vec<_>>()
+                .join("\n")
+        }
+    }
 }
